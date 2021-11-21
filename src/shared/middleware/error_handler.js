3229364301f645e14f491acb37ff4b26196eaf6e
David Ngugi <ndavidngugi@gmail.com>
2021-11-22 00:21:32 +0300

    Update error handler response

--- src/shared/middleware/error_handler.js
+++ src/shared/middleware/error_handler.js
@@ -1,9 +1,12 @@
+const { jsonify } = require('../../shared/utils');
+
 const error_handler = (err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     // send error message
-    res.send(err.message).status(err.status || 500);
+    err.status = err.status ? err.status : 500;
+    res.send(jsonify(err.message)).status(err.status);
 }
 
 module.exports = error_handler;
